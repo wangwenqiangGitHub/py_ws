7205c6e31de789ab0e68ae6b538e24ef5e223f8c
wangwenqiangGitHub <1103169969@qq.com>
2021-12-06 13:02:55 +0800

    ”调试例子1完成“

--- example1/tesPyQt.py
+++ example1/tesPyQt.py
@@ -1,18 +1,22 @@
+# -*- coding: utf-8 -*-
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from functools import partial
 import testui
 
-def convert(ui):
-    input = ui.lineEdit.txt() #获取第一个文本编辑框的数字
-    result = float(input) * 6.5
-    ui.lineEdit_2.setText(str(result)) # 在第二个文本框中显示
 
-if __name__ == '__main__':
+def convert(inputui):
+    input = inputui.lineEdit.text()
+    result = float(input) * 6.5
+    inputui.lineEdit_2.setText(str(result))
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
     ui = testui.Ui_MainWindow()
     ui.setupUi(MainWindow)
 
     ui.pushButton.clicked.connect(partial(convert, ui))
+    MainWindow.show()
     sys.exit(app.exec_())
